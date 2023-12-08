47184914597feac24c44018bcbb4f12bc2026d68
Kelvin <kelvin@Kelvins-MacBook-Air.local>
2023-12-08 20:27:13 +0700

    initial commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import { CarouselProduct } from "./";
+
+const HomePage = () => {
+  return (
+    <div className="bg-amazonclone-background">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto">
+        <CarouselProduct />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as HomePage } from "./HomePage";
+export { default as CarouselProduct } from "./CarouselProduct";
+export { default as NavBar } from "./NavBar";
+export { default as Search } from "./Search";
+export { default as Checkout } from "./Checkout";
+export { default as ProductPage } from "./ProductPage";
+export { default as SearchResults } from "./SearchResults";
+export { default as ProductDetails } from "./ProductDetails";
+export { default as ProductBadge } from "./ProductBadge";
+export { default as ProductRatings } from "./ProductRatings";
